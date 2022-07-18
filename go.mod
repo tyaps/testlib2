694dea9e3deaf8559c2ff9fc78e058a6a5a5a3fa
Sergey Tyapkin <s.tyapkin@omprussia.ru>
2022-07-18 14:20:43 +0300

    33

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/tyaps/testlib2/v2
+module github.com/tyaps/testlib2/v3
 
 go 1.18
--- lib2.go
+++ lib2.go
@@ -2,6 +2,6 @@ package testlib2
 
 import "fmt"
 
-func PrintName(s string) {
-	fmt.Printf("lib2 v2 prints: %s", s)
+func PrintName(s, s2 string) {
+	fmt.Printf("lib2 v3 prints: %s and %s", s, s2)
 }
